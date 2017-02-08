543db8f473db54c32aa5a73dd2c4bf12321c32c1
Lindsey Sewell <lindsey.sewell@umontana.edu>
2017-02-08 10:10:18 -0700

    empty examples and practices

---
+++ inclass/01-helloworld/sketch.js
@@ -0,0 +1,7 @@
+function setup() {
+  console.log ("Hello Errybody")
+}
+
+function draw() {
+
+}
---
+++ inclass/02-helloworld/sketch.js
@@ -0,0 +1,11 @@
+function setup() {
+  createCanvas(500, 500);
+    background(255, 100, 200);
+    fill(5, 26, 134);
+  ellipse(250, 250, 40, 40);
+
+}
+
+function draw() {
+
+}
